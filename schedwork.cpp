4396768b0d40ff5286760bcfcf5051b163fe2f11
Anika Bose <arbose@usc.edu>
2025-04-24 16:36:06 -0700

    hw5

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,14 +21,24 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
+bool scheduleHelper(
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    size_t day,
+    std::vector<size_t> shiftTrack, // tracks how many total shifts each worker is scheduled for, using 0-based indexing
+    size_t n,
+    size_t k
+);
 
 
 // Add your implementation of schedule() and other helper functions here
 
 bool schedule(
     const AvailabilityMatrix& avail,
-    const size_t dailyNeed,
-    const size_t maxShifts,
+    const size_t dailyNeed, // d 
+    const size_t maxShifts, // m
     DailySchedule& sched
 )
 {
@@ -37,9 +47,76 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
+    size_t n = avail.size(); // days
+    size_t k = avail[0].size(); // workers
 
 
 
+    std::vector<size_t> shiftTracker;
+    for (size_t i = 0; i < k; i++){
+        shiftTracker.push_back(0);
+    }
 
+    for (size_t i = 0; i < n; i++) {
+        // sched passed through schedule has no specific size
+        // update so it is the length of days
+        vector<Worker_T> temp;
+        sched.push_back(temp);
+    }
+
+    // pass through with day as 0, using 0-based indexing
+    return scheduleHelper(avail, dailyNeed, maxShifts, sched, 0, shiftTracker, n, k);
+
+}
+
+bool scheduleHelper(
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    size_t day,
+    std::vector<size_t> shiftTrack, // tracks how many total shifts each worker is scheduled for, using 0-based indexing
+    size_t n,
+    size_t k
+){
+    
+    // we have reached the end of the schedule, i.e. no more days that need to be filled
+    // recall: using 0-based indexing for day, and n is avail.size()
+    if (day >= n){ 
+        return true;    
+    }
+
+    // number of nurses scheduled on the day meet dailyNeed, recurse to the next day in the schedule that needs to be filled
+    if (sched[day].size() >= dailyNeed){
+        return scheduleHelper(avail, dailyNeed, maxShifts, sched, day + 1, shiftTrack, n, k);
+    }   
+
+
+    // loop through all workers 
+    for (size_t i = 0; i < k; i++){
+        // conditional to check if worker i has not exceeded their shift count and are available that day to work
+        // checks through schedule to ensure worker has not already been booked for the day
+        
+        if (shiftTrack[i] < maxShifts && avail[day][i] == true && std::find(sched[day].begin(), sched[day].end(), i) == sched[day].end()){
+            // add nurse to the vector of nurses working that day
+            sched[day].push_back(i);
+            
+            // increase their tracker by 1 shift
+            shiftTrack[i]++;
+
+            // recursively call to ensure day has been been filled , if false then backtrack
+            if (scheduleHelper(avail, dailyNeed, maxShifts, sched, day, shiftTrack, n, k) == true){
+                return true;
+            }
+
+            // backtrack by deleting worker from the day's schedule and updating their shift count
+            sched[day].pop_back();
+            shiftTrack[i]--;
+
+        }
+    }
+
+    // doesn't work
+   return false;
 }
 
--- wordle.cpp
+++ wordle.cpp
@@ -2,7 +2,7 @@
 // For debugging
 #include <iostream>
 // For std::remove
-#include <algorithm> 
+// #include <algorithm> 
 #include <map>
 #include <set>
 #endif
@@ -13,7 +13,13 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+void wordleHelper(
+    std::string word, // current word being created
+    std::string floating, // available letters to use
+    const std::set<std::string>& dict, // all valid words in English language
+    std::set<std::string>& final, // final list of all possible words
+    int index // current letter position in the word
+);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,7 +28,71 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+    std::set<std::string> potentialWords;
+    //int wordLength = in.length();
 
+    // start at index = 0 because starting at the beginning of the word
+    wordleHelper(in, floating, dict, potentialWords, 0);
+
+    // return final set 
+    return potentialWords;
 }
 
+
 // Define any helper functions here
+
+void wordleHelper(
+    std::string word,
+    std::string floating,
+    const std::set<std::string>& dict,
+    std::set<std::string>& final,
+    int index
+){
+    // reached the end of the word, i.e. recursed through the entire word
+    if (index == word.size()){
+        
+        // if word is found in set of all possible English words and all of the needed floating letters have been used, then this is a valid guess
+        if (dict.find(word) != dict.end() && floating.empty()){
+            final.insert(word);
+        }
+
+        // exit out
+        return;
+    }
+
+
+    // int wordLength = word.length();
+
+    // checks if letter has been placed into spot, if so, move onto next index/position in the word
+    if (word[index] != '-'){
+        wordleHelper(word, floating, dict, final, index + 1);
+        return;
+    }
+
+    // loops through floating letters, placing it in the "-" spot starting with the first floating letter 
+    for (int i = 0; i < floating.length(); i++){
+        word[index] = floating[i];
+        // calls loop onto next position in the word
+        // decrease size of floating string as letters are used up
+        wordleHelper(word, floating.substr(0, i) + floating.substr(i + 1), dict, final, index + 1);
+    }
+
+    // has gone through floating letters, now floating letter amount should be zero as for loop is not entered
+    // gets how many dashes/ blanks are still left
+    int numDashes = 0;
+    for (int i = index; i < word.size(); i++){
+        if (word[i] == '-'){
+            numDashes++;
+        }
+    }
+
+    // only calls this recursive statement if there are still more blanks to fill
+    if (numDashes >= floating.length()){
+        for (char curr = 'a'; curr <= 'z'; curr++){
+            // loops through all letters, even ones that we already checked in floating, because there could be duplicates
+            word[index] = curr;
+            // recurse by moving onto next position in the word
+            wordleHelper(word, floating, dict, final, index + 1);
+        }
+    }
+}
